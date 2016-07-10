94c0bb9225a1c0ea2a7f2bc8e5ce6ca3fe043632
ciffi <alessio@ciffi.it>
2016-07-10 13:55:23 +0200

    fix setup config generation

--- webpack/dev/scripts/config/config.js
+++ webpack/dev/scripts/config/config.js
@@ -1,3 +1,31 @@
-var Config = '@REPLACE__CONFIG@';
+var Config = {
+	projectName: '@REPLACE__CONFIG@',
+	envs: {
+		dev: {
+			baseUrl: 'http://localhost',
+			apiUrl: 'http://@REPLACE__CONFIG@.local/app_dev.php',
+			shareUrl: 'http://localhost',
+			assetsUrl: '/dev/'
+		},
+		local: {
+			baseUrl: 'http://@REPLACE__CONFIG@.local',
+			apiUrl: 'http://@REPLACE__CONFIG@.local/app_dev.php',
+			shareUrl: 'http://@REPLACE__CONFIG@.local/app_dev.php',
+			assetsUrl: 'http://@REPLACE__CONFIG@.local/assets/'
+		},
+		stage: {
+			baseUrl: 'http://@REPLACE__CONFIG@.ppreview.it',
+			apiUrl: 'http://@REPLACE__CONFIG@.ppreview.it/app_stage.php',
+			shareUrl: 'http://@REPLACE__CONFIG@.ppreview.it/app_stage.php',
+			assetsUrl: 'http://@REPLACE__CONFIG@.ppreview.it/assets/'
+		},
+		prod: {
+			baseUrl: 'http://www.@REPLACE__CONFIG@.it',
+			apiUrl: 'http://www.@REPLACE__CONFIG@.it',
+			shareUrl: 'http://www.@REPLACE__CONFIG@.it',
+			assetsUrl: 'http://www.@REPLACE__CONFIG@.it/assets/'
+		}
+	}
+};
 
 module.exports = Config;
