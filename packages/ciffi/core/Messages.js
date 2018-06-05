0f67644d4dc42ef79c647f3cf2ad957ce8a935f2
ciffi <alessio@ciffi.it>
2018-06-05 13:23:09 +0200

    [ciffi] - refactoring - available task: setup / dev-old / build-old

--- packages/ciffi/core/Messages.js
+++ packages/ciffi/core/Messages.js
@@ -1,11 +1,12 @@
 const chalk = require('chalk');
+const pkg = require('../package.json');
 
 const showCommandErrorMessage = () => {
   console.log('');
   console.log('');
   console.log(chalk.red.bold('Command not found'));
   console.log('');
-  console.log(chalk.blue('ciffi -h') + chalk.green(' -- core list --'));
+  console.log(chalk.cyan('ciffi -h') + chalk.green(' -- core list --'));
   console.log('');
 };
 
@@ -13,7 +14,7 @@ const showGreetings = () => {
   console.log('');
   console.log('');
   showCommands();
-  console.log(chalk.blue.bold(pkg.author.name) + chalk.blue.bold(' ^_^'));
+  console.log(chalk.cyan.bold(pkg.author.name) + chalk.cyan.bold(' ^_^'));
   console.log('');
 };
 
@@ -23,7 +24,7 @@ const showCommandListMsg = () => {
   console.log(chalk.green.bold('-- Ciffi Frontend Generator --'));
   console.log('');
   console.log('');
-  console.log('Available core:');
+  console.log('Available tasks:');
   console.log('');
   showCommands();
   console.log('');
@@ -55,34 +56,23 @@ const showLogo = () => {
 };
 
 const showCommands = () => {
-  console.log(chalk.blue('ciffi setup projectname') + chalk.green(' -- create new project --'));
+  console.log(chalk.cyan('npm start') + chalk.green(' -- start webpack dev server and generate dev build with watch --'));
   console.log('');
-  console.log(chalk.blue('ciffi dev') + chalk.green(' -- start livereload server and generate local build with watch --'));
+  console.log(chalk.cyan('npm run dev') + chalk.green(' -- start livereload server and generate dev build with watch --'));
   console.log('');
-  console.log(chalk.blue('ciffi build') + chalk.green(' -- generate build --'));
+  console.log(chalk.cyan('npm run build [env]') + chalk.green(' -- generate build (use defaultBuildEnv from .ciffisettings if env is not specified) --'));
   console.log('');
-  console.log(chalk.blue('ciffi e2e') + chalk.green(' -- e2e test with nightwatch and cucumber (default configuration) --'));
+  console.log(chalk.cyan('npm run assets') + chalk.green(' -- copy all assets into build folder --'));
   console.log('');
-  console.log(chalk.blue('ciffi e2e chrome //default, chrome or firefox') + chalk.green(' -- e2e test with nightwatch and cucumber (custom configuration) --'));
+  console.log(chalk.cyan('npm run config [env]') + chalk.green(' -- generate config.js (use defaultBuildEnv from .ciffisettings if env is not specified) --'));
   console.log('');
-  console.log(chalk.blue('ciffi newpage pagename') + chalk.green(' -- create new html and js page --'));
-  console.log('');
-  console.log(chalk.blue('ciffi newmodule modulename') + chalk.green(' -- create new js module --'));
-  console.log('');
-  console.log(chalk.blue('ciffi newcomponent componentname') + chalk.green(' -- create new js component --'));
-  console.log('');
-  console.log(chalk.blue('ciffi jsdoc') + chalk.green(' -- generate javascript documentation with jsdoc into ./jsdoc path --'));
-  console.log('');
-  console.log(chalk.blue('ciffi cssdoc') + chalk.green(' -- generate sass documentation with sassdoc into ./cssdoc path --'));
-  console.log('');
-  console.log(chalk.blue('ciffi styleguide') + chalk.green(' -- generate style guides with kss into ./styleguide path --'));
+  console.log(chalk.cyan('npm test') + chalk.green(' -- test your code with jest --'));
   console.log('');
 };
 
 module.exports = {
   showCommandErrorMessage,
   showCommandListMsg,
-  showCommands,
   showGreetings,
   showLogo
 };
