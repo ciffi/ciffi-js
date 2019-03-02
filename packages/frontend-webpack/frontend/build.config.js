2035cc88f98f1f072089d99c14e647e5c9fa785e
ciffi <alessio@ciffi.it>
2019-03-02 12:46:48 +0100

    [frontend] - fix terser configuration

--- packages/frontend-webpack/frontend/build.config.js
+++ packages/frontend-webpack/frontend/build.config.js
@@ -1,14 +1,9 @@
 const path = require('path');
 const ConfigFile = require(__dirname + "/.ciffisettings");
-const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
 const baseConfig = require("./config/webpack");
-const minifyOpts = {
-  uglifyOptions: {
-    sourceMap: false,
-    comments: false
-  }
-};
-const plugins = [new UglifyJSPlugin(minifyOpts)]
+
+const plugins = []
 
 if (ConfigFile.general.offline) {
   plugins.push(baseConfig.plugins[0])
@@ -23,6 +18,18 @@ module.exports = {
   mode: "production",
   performance: {
     hints: false
+  },
+  optimization: {
+    ...baseConfig.optimization,
+    minimizer: [
+      new TerserPlugin({
+        test: /\.js(\?.*)?$/i,
+        cache: true,
+        parallel: true,
+        sourceMap: false,
+        terserOptions: {}
+      })
+    ]
   },
   entry: {
     main: path.join(__dirname, ConfigFile.build.srcPathName, "scripts", "main.js")
