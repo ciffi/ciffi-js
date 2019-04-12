45498f8785b25aac8562146d7c1f2bfdc2bfc784
ciffi <alessio@ciffi.it>
2019-04-12 16:06:24 +0200

    [builder] - removed single line log

--- packages/builder/task/Assets.js
+++ packages/builder/task/Assets.js
@@ -1,80 +1,79 @@
-const chalk = require('chalk');
-const fileExists = require('file-exists');
-const exec = require('child_process').exec;
-const Log = require('single-line-log').stdout;
-const path = require('path');
-const ConfigFile = path.join(process.cwd(), '.ciffisettings');
+const chalk = require('chalk')
+const fileExists = require('file-exists')
+const exec = require('child_process').exec
+const path = require('path')
+const ConfigFile = path.join(process.cwd(), '.ciffisettings')
 
-const emptyCallback = () => {};
+const emptyCallback = () => {}
 
 class Assets {
   constructor(callback = emptyCallback) {
     if (fileExists.sync(ConfigFile)) {
-      this.config = require(ConfigFile);
+      this.config = require(ConfigFile)
     } else {
       console.error(
         chalk.red.bold('☠️ Project build failed:') +
           ' ' +
           chalk.blue("can't find .ciffisettings file ☠️")
-      );
-      return console.log('');
+      )
+      return console.log('')
     }
 
     if (!this.getAssets()) {
-      callback();
-      return;
+      callback()
+      return
     }
 
-    let process = exec(this.getAssets());
+    let process = exec(this.getAssets())
 
     process.stdout.on('data', res => {
       if (res.indexOf('ERROR in') >= 0 || res.indexOf('Error:') >= 0) {
-        console.log(chalk.red(res));
+        console.log(chalk.red(res))
       } else {
-        Log('🦄 ' + chalk.blue(res));
+        console.log('🦄 ' + chalk.blue(res))
       }
-    });
+    })
 
     process.stderr.on('data', res => {
       if (res.indexOf('ERROR in') >= 0 || res.indexOf('Error:') >= 0) {
-        console.log(chalk.red(res));
+        console.log(chalk.red(res))
       } else {
-        Log('🦄 ' + chalk.blue(res));
+        console.log('🦄 ' + chalk.blue(res))
       }
-    });
+    })
 
     process.on('close', res => {
       if (res === 0) {
-        Log(
+        console.log(
           chalk.blue('🦄 Assets copied in ') +
             ' ' +
             this.config.build.path +
             '/ ' +
             chalk.green.bold(' OK')
-        );
-        console.log('');
-        callback();
+        )
+        console.log('')
+        callback()
       }
-    });
+    })
   }
 
   getAssets() {
-    const staticFolders = this.config.general.staticFolders;
-    const staticFiles = this.config.general.staticFiles;
+    const staticFolders = this.config.general.staticFolders
+    const staticFiles = this.config.general.staticFiles
     const assetPath =
       process.platform === 'win32'
         ? this.config.build.path.replace(/\//g, '\\')
-        : this.config.build.path;
-    const assetPathName = this.config.build.srcPathName;
+        : this.config.build.path
+    const assetPathName = this.config.build.srcPathName
     const pathsArray =
-      staticFolders && staticFolders.length ? staticFolders : false;
-    const filesArray = staticFiles && staticFiles.length ? staticFiles : false;
+      staticFolders && staticFolders.length ? staticFolders : false
+    const filesArray = staticFiles && staticFiles.length ? staticFiles : false
 
     if (filesArray) {
-      let temp = '';
+      let temp = ''
 
       for (let i = 0; i < filesArray.length; i++) {
-        temp += "'" + path.join(assetPathName, filesArray[i]) + "' ";
+        temp += "'" + path.join(assetPathName, filesArray[i]) + "' "
       }
 
       exec(
@@ -83,26 +82,26 @@ class Assets {
           '.bin',
           'copyfiles'
         )} -f ${temp} ${assetPath}`
-      );
+      )
     }
 
     if (pathsArray) {
-      let temp = '';
+      let temp = ''
 
       for (let i = 0; i < pathsArray.length; i++) {
         temp +=
-          "'" + path.join(assetPathName, pathsArray[i], '**', '*.*') + "' ";
+          "'" + path.join(assetPathName, pathsArray[i], '**', '*.*') + "' "
       }
 
       return `${path.join(
         'node_modules',
         '.bin',
         'copyfiles'
-      )} -u 1 ${temp} ${assetPath}`;
+      )} -u 1 ${temp} ${assetPath}`
     }
 
-    return;
+    return
   }
 }
 
-module.exports = Assets;
+module.exports = Assets
--- packages/builder/utils/Logger.js
+++ packages/builder/utils/Logger.js
@@ -1,9 +1,7 @@
-const chalk = require('chalk');
-const Log = require('single-line-log').stdout;
-const Notify = require('../task/Notify');
+const chalk = require('chalk')
+const Notify = require('../task/Notify')
 
 class Logger {
-  
   constructor(processes, prefix = '') {
     for (let i = 0; i < processes.length; i++) {
       processes[i].stdout.on('data', res => {
@@ -13,19 +11,17 @@ class Logger {
           res.indexOf('error ') >= 0 ||
           res.indexOf('Errors:') >= 0
         ) {
-          console.log(
-            chalk.bgRed(' ' + prefix + ' -->') + ' ' + chalk.red(res)
-          );
-          Notify.sendObjError(res);
+          console.log(chalk.bgRed(' ' + prefix + ' -->') + ' ' + chalk.red(res))
+          Notify.sendObjError(res)
         } else {
-          Log(chalk.green(prefix + ' --> ') + chalk.blue(res));
-          
+          console.log(chalk.green(prefix + ' --> ') + chalk.blue(res))
+
           if (res.indexOf('Built at: ') >= 0 || res.indexOf('Built in ') >= 0) {
-            Notify.sendReady('🏗 DEV ready - click to open');
+            Notify.sendReady('🏗 DEV ready - click to open')
           }
         }
-      });
-      
+      })
+
       processes[i].stderr.on('data', res => {
         if (
           res.indexOf('ERROR in') >= 0 ||
@@ -33,22 +29,20 @@ class Logger {
           res.indexOf('error ') >= 0 ||
           res.indexOf('Errors:') >= 0
         ) {
-          console.log(
-            chalk.bgRed(' ' + prefix + ' -->') + ' ' + chalk.red(res)
-          );
-          Notify.sendObjError(chalk(res));
+          console.log(chalk.bgRed(' ' + prefix + ' -->') + ' ' + chalk.red(res))
+          Notify.sendObjError(chalk(res))
         } else {
-          Log(chalk.green(prefix + ' --> ') + chalk.blue(res));
+          console.log(chalk.green(prefix + ' --> ') + chalk.blue(res))
         }
-      });
-      
+      })
+
       processes[i].on('close', res => {
         if (res !== 0) {
-          Log(chalk.green(prefix + ' --> ') + chalk.green(res));
+          console.log(chalk.green(prefix + ' --> ') + chalk.green(res))
         }
-      });
+      })
     }
   }
 }
 
-module.exports = Logger;
+module.exports = Logger
